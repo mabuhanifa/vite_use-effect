959d66af5977f37cbf718eca0dcbd933929ce332
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-10 21:16:36 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import MyComponent from "./components/MyComponent";
+import MyComponentClass from "./components/MyComponentClass";
 
 function App() {
   return (
     <div>
       <MyComponent />
+      <MyComponentClass />
     </div>
   );
 }
