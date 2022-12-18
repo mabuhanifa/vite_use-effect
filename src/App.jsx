0f9f05a66b750566a7c1e0d23e27f97342120066
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-18 17:30:21 +0600

    Times Up

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
   // const myOption = useMemo(() => ({ url }), [url]);
   // myOption is a object with url property
   // const { data } = useFetch(myOption);
-
   const { data } = useFetch({ url, onSuccess: () => console.log("success") });
-  const count = useStopwatch()
+  const count = useStopwatch(5);
   return (
     <div className="main">
       {/* <MyComponent /> */}
       {/* <MyComponentClass /> */}
-      {count}
+      {count > 0 ? count : "0"} {}
+      {count <= 0 && "Times Up"}
       <div>
         {JSON.stringify(data)}
 
--- src/hooks/useStopwatch.jsx
+++ src/hooks/useStopwatch.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 
-export default function useStopwatch() {
-  const [count, setCount] = useState(0);
+export default function useStopwatch(time) {
+  const [count, setCount] = useState(time);
   useEffect(() => {
     const interval = setInterval(() => {
-      setCount((p) => p + 1);
+      if (count>=0) {
+        setCount(count - 1);
+      }
     }, 1000);
     return () => clearInterval(interval);
-  }, []);
+  }, [count]);
 
   return count;
 }
