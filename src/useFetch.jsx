5820714413daa5e5a2a76b827368e1d2d713ece0
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-18 16:03:51 +0600

    async fetcher

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -4,13 +4,21 @@ export default function useFetch(options) {
   const { url } = options;
 
   const [data, setData] = useState(null);
+  const fetcher = async () => {
+    if (url) {
+      const res = await fetch(url);
+      const newData = await res.json();
+      setData(newData);
+    }
+  };
 
   useEffect(() => {
-    if (url) {
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => setData(data));
-    }
+    // if (url) {
+    //   fetch(url)
+    //     .then((res) => res.json())
+    //     .then((data) => setData(data));
+    // }
+    fetcher();
     console.log("useEffect rendering the app");
   }, [url]);
 
