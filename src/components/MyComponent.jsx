c1891db15985880b8435b1049560fc8175171e1e
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-18 15:21:16 +0600

    updating

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -2,24 +2,32 @@ import React, { useEffect, useState } from "react";
 
 const MyComponent = () => {
   const [count, setCount] = useState(0);
+
   const [date, setDate] = useState(new Date());
+
   const incoming = "incoming_ports";
+
   const tick = () => {
     setDate(new Date());
   };
+
   useEffect(() => {
     document.title = `Clicked ${count} times`;
   }, [count]);
 
   useEffect(() => {
     console.log("starting timer");
+
     const interval = setInterval(tick, 1000);
+
     // do the cleanup - stop the timer
+
     return () => {
       console.log("component unmounted");
       clearInterval(interval);
     };
   }, []);
+
   return (
     <div style={{ margin: "50px" }}>
       <p>Time: {date.toLocaleTimeString()}</p>
